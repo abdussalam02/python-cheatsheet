7f683ec4b5f930feb535fd6b5148a48bf1e118b1
huhuhang <huhuhang@users.noreply.github.com>
2025-11-19 14:00:57 +0800

    Remove custom scrollbar styles from App.vue to streamline CSS and improve maintainability. fix https://github.com/labex-labs/python-cheatsheet/issues/505

--- src/App.vue
+++ src/App.vue
@@ -62,34 +62,3 @@ if (gTag && gTag !== 'tag' && gTag.trim() !== '') {
   </BaseBanner>
   <RouterView />
 </template>
-
-<style>
-::-webkit-scrollbar {
-  width: 8px;
-}
-
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: #868686;
-}
-
-/* Track */
-.dark ::-webkit-scrollbar-track {
-  background: #374151;
-}
-
-/* Handle */
-.dark ::-webkit-scrollbar-thumb {
-  background: #6b7280;
-}
-
-/* Track */
-::-webkit-scrollbar-track {
-  background: #dfe0e2;
-}
-
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: #9ca3af;
-}
-</style>
